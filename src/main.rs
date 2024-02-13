43d478cb37f4b5d9a6d8821497077f9d74a3c563
gb10udn <rbbpx0926@gmail.com>
2024-02-13 23:20:51 +0900

    ファイルパスからテキストファイルを開いて、改行コードで split するコードを追加した。

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "./misc/sample_001.py";
+    let mut f = File::open(path).unwrap();
+    let mut result = String::new();
+    f.read_to_string(&mut result).unwrap();
+    println!("{}", result);
+
+    let lines: Vec<&str> = result.split("\r\n").collect();
+    println!("{:?}", lines);
 }
 
-fn remove_head_and_tail_double_quotation(arg: String) -> String {
+fn remove_head_and_tail_double_quotation(arg: &String) -> String {
     let mut result = arg.clone();
     if result.ends_with("\n") == true {
         result.pop();  // INFO: 240113 標準入力で取得時の末尾の改行コードを除去するため。
@@ -22,7 +32,7 @@ mod tests {
     fn test_remove_head_and_tail_double_quotation() {
         use crate::remove_head_and_tail_double_quotation;
 
-        assert_eq!(remove_head_and_tail_double_quotation(String::from("abc\n")), String::from("abc"));
-        assert_eq!(remove_head_and_tail_double_quotation(String::from("\"abc\"\n")), String::from("abc"));
+        assert_eq!(remove_head_and_tail_double_quotation(&String::from("abc\n")), String::from("abc"));
+        assert_eq!(remove_head_and_tail_double_quotation(&String::from("\"abc\"\n")), String::from("abc"));
     }
 }
