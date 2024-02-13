1755db636b1449849981731eed5a8c73cb10e079
gb10udn <rbbpx0926@gmail.com>
2024-02-13 22:52:42 +0900

    add function of remove_head_and_tail_double_quotation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn remove_head_and_tail_double_quotation(arg: String) -> String {
+    let mut result = arg.clone();
+    if result.ends_with("\n") == true {
+        result.pop();  // INFO: 240113 標準入力で取得時の末尾の改行コードを除去するため。
+    }
+    if result.starts_with("\"") {
+        result.remove(0);
+    }
+    if result.ends_with("\"") {
+        result.pop();
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_remove_head_and_tail_double_quotation() {
+        use crate::remove_head_and_tail_double_quotation;
+
+        assert_eq!(remove_head_and_tail_double_quotation(String::from("abc\n")), String::from("abc"));
+        assert_eq!(remove_head_and_tail_double_quotation(String::from("\"abc\"\n")), String::from("abc"));
+    }
+}
