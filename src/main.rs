d952ba60a741cb62cd3af4d4a4cf06c30bb075fa
gb10udn <rbbpx0926@gmail.com>
2024-02-18 00:36:45 +0900

    docstring を削除する機能を追加した。

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,21 @@ use std::fs::File;
 use regex::Regex;
 
 fn main() {
-    let path = String::from("./misc/sample_001.py");
-    let path = remove_head_and_tail_double_quotation(&path);
-    let code = open_file(&path);
-    let code_ = remove_comment_py(&code, vec!["TODO:", "FIXME:"]);
 
-    println!("{}", code_);
+    let path = "./misc/sample_001.py";
+    let rm_docstring = true;
+
+    let path = String::from(path);  // TODO: 240217 ベースディレクトリを指定すると、その配下の .py ファイルを対象とするようにする。
+    let path = remove_head_and_tail_double_quotation(&path);
+
+
+    let mut code = open_file(&path);
+    code = remove_comment_py(&code, vec!["TODO:", "FIXME:", "EDIT:", "HACK:", "[START]", "[END]"]);
+
+    if rm_docstring {
+        code = remove_docstring_py(&code);
+    }
+    println!("{}", code);  // TODO: 240217 ファイルに書き込む。
 }
 
 fn open_file(path: &String) -> String {
@@ -24,9 +33,14 @@ fn remove_comment_py(src: &String, targets: Vec<&str>) -> String {
         .map(|keyword| format!("#\\s*{}.*", keyword))
         .collect::<Vec<String>>()
         .join("|");
-    let regex = Regex::new(&pattern).unwrap();
-    println!("{:?}", pattern);
-    regex.replace_all(src, "").to_string()
+    let re = Regex::new(&pattern).unwrap();
+    re.replace_all(src, "").to_string()
+}
+
+fn remove_docstring_py(src: &String) -> String {
+    let re = Regex::new(r#"\s*?"{3}[\s\S]*?"{3}|'\s*?{3}[\s\S]*?'{3}"#).unwrap();  // INFO: 240218 \s は空白や改行コード。\S はそれ以外。*? は非貪欲マッチ。
+    let result = re.replace_all(src, "");
+    result.to_string()
 }
 
 fn remove_head_and_tail_double_quotation(arg: &String) -> String {
@@ -70,5 +84,34 @@ mod tests {
         "#;
 
         assert_eq!(remove_comment_py(&src.to_string(), vec!["TODO:", "FIXME:"]), dst.to_string());
+    }
+
+    #[test]
+    fn test_remove_docstring_py() {
+        use crate::remove_docstring_py;
+
+        let src = r#"
+            import datetime
+            def hogeee():
+                """
+                this is docstring !!!
+                """
+                return datetime.datetime.now()
+            def piyo():
+                """
+                the second docstring !!!
+                """
+                return 123
+        "#;
+
+        let dst = r#"
+            import datetime
+            def hogeee():
+                return datetime.datetime.now()
+            def piyo():
+                return 123
+        "#;
+
+        assert_eq!(remove_docstring_py(&src.to_string()), dst.to_string());
     }
 }
