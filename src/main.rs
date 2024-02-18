740fd8d63414c20ecd8fa04190a473ecdb71ad37
gb10udn <rbbpx0926@gmail.com>
2024-02-18 10:29:59 +0900

    # TODO: の前に付いた space も削除できるようにした。

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,19 @@ use std::fs::File;
 use regex::Regex;
 
 fn main() {
-
     let path = "./misc/sample_001.py";
     let rm_docstring = true;
 
     let path = String::from(path);  // TODO: 240217 ベースディレクトリを指定すると、その配下の .py ファイルを対象とするようにする。
     let path = remove_head_and_tail_double_quotation(&path);
 
-
     let mut code = open_file(&path);
-    code = remove_comment_py(&code, vec!["TODO:", "FIXME:", "EDIT:", "HACK:", "[START]", "[END]"]);
+    code = remove_comment_py(&code, vec!["TODO:", "FIXME:", "EDIT:", "HACK:", "INFO:", "[START]", "[END]"]);
 
     if rm_docstring {
         code = remove_docstring_py(&code);
     }
-    println!("{}", code);  // TODO: 240217 ファイルに書き込む。
+    println!("{}", code);  // TODO: 240217 ファイルに書き込む。 (requirements.txt を元に環境を作って、pyinstaller でビルドまでできると、いいかも？)
 }
 
 fn open_file(path: &String) -> String {
@@ -27,17 +25,17 @@ fn open_file(path: &String) -> String {
     result
 }
 
-fn remove_comment_py(src: &String, targets: Vec<&str>) -> String {
+fn remove_comment_py(src: &String, targets: Vec<&str>) -> String {  // TODO: 240218 ps / vba にも対応すること。(vba を引き抜いて変換して、また、.xlsm に戻すのもいいかも？)
     let pattern = targets
         .iter()
-        .map(|keyword| format!("#\\s*{}.*", keyword))
+        .map(|keyword| format!(r"\s*?#\s*?{}.*", keyword))
         .collect::<Vec<String>>()
         .join("|");
     let re = Regex::new(&pattern).unwrap();
-    re.replace_all(src, "").to_string()
+    re.replace_all(src, "").to_string()  // TODO: 240218 削除後に空白のみになった場合、その行を削除する？
 }
 
-fn remove_docstring_py(src: &String) -> String {
+fn remove_docstring_py(src: &String) -> String {  // HACK: 240218 関数名は docstring とあるが、実際に、３つのクオーテーションを落とすので、そういった関数名にすること。
     let re = Regex::new(r#"\s*?"{3}[\s\S]*?"{3}|\s*?'{3}[\s\S]*?'{3}"#).unwrap();  // INFO: 240218 \s は空白や改行コード。\S はそれ以外。*? は非貪欲マッチ。
     let result = re.replace_all(src, "");
     result.to_string()
@@ -79,8 +77,8 @@ mod tests {
 
         let dst = r#"
             import os
-            print('始めるよ！')  
-            os.path.basename("hoge")  
+            print('始めるよ！')
+            os.path.basename("hoge")
         "#;
 
         assert_eq!(remove_comment_py(&src.to_string(), vec!["TODO:", "FIXME:"]), dst.to_string());
